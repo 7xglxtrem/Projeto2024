05c31b9ca0215838f36bbaf2492c5ed0b8b78200
7xglxtrem <126262446+7xglxtrem@users.noreply.github.com>
2024-04-17 13:58:59 -0300

    Update funcoes_javascript.js

--- funcoes_javascript.js
+++ funcoes_javascript.js
@@ -4,9 +4,9 @@
 function dividir (num1, num2){
      return num1/num2;
    }
-function subtração (num1, num2){
+function subtraçao (num1, num2){
      return num1-num2;
    }
-function multiplicação (num1, num2){
+function multiplicaçao (num1, num2){
      return num1*num2;
    }
