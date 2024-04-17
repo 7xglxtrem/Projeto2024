89f18e37bab88518782d3d5f5a6653687644f002
7xglxtrem <126262446+7xglxtrem@users.noreply.github.com>
2024-04-17 13:58:12 -0300

    Create funcoes_javascript.js

---
+++ funcoes_javascript.js
@@ -0,0 +1,12 @@
+ function somar (num1, num2){
+     return num1+num2;
+   }
+function dividir (num1, num2){
+     return num1/num2;
+   }
+function subtração (num1, num2){
+     return num1-num2;
+   }
+function multiplicação (num1, num2){
+     return num1*num2;
+   }
